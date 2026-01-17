6519eabf93bd9cdc4ead1525999e459d1496fca0
chhuiling <chenalicen@gmail.com>
2026-01-17 11:52:14 +0100

    juntar servos

---
+++ controllers/mover_servo_ct15/mover_servo_ct15.py
@@ -0,0 +1,16 @@
+from controller import Robot, Motor
+import math
+
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
+
+aspa_motor = robot.getDevice("aspa_motor_ct15")
+
+# Convertir 51.43 grados a radianes
+target_pos = math.radians(51.43)
+
+aspa_motor.setVelocity(2.0)
+aspa_motor.setPosition(target_pos)
+
+while robot.step(timestep) != -1:
+    pass
